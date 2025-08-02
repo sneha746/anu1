44bdcf278d2639a364378ed26efa297edaea25d5
sneha746 <snehahari759@gmail.com>
2025-08-02 18:43:18 +0530

    printing

--- app.py
+++ app.py
@@ -1,16 +1,1 @@
-print("HELLO WORLD")
-A=["halloo",123]
-for i in A:
-     print(i)
-B=["sneha",1234]
-print(B)
-a=B[0]
-print(a)
-dict1={"a":20,"b":21,"c":22}
-for key,value in dict1.items():
-     print(key,value)
-print(len(B))
-x=6
-while 6<len(B):
-    print("HII")
-    break
+print("hello")
